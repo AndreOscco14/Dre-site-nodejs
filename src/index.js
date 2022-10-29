d9a9ed7a695d7da9c5155f7b42b3c6d1b77f8b56
bigDre82 <andre.oscco.14@gmail.com>
2022-10-29 23:03:11 +0200

    puerto actualizado

--- src/index.js
+++ src/index.js
@@ -25,5 +25,5 @@ app.use(indexRoutes);
 // static files
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(3000);
-console.log('Servidor desplegado on port', 3000);
+app.listen(process.env.PORT || 3000);
+console.log('Servidor desplegado on port', process.env.PORT || 3000);
